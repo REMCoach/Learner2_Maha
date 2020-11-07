93f67237f5a993bb8dd9dc51a7d3b0cfc07e536b
mahalakshmi111 <74039533+mahalakshmi111@users.noreply.github.com>
2020-11-07 10:30:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = require("./app");
 
 const http = require("http");
 const server = http.Server(app);
-
+console.log(server);
 server.listen(PORT, HOST, () =>
   console.log(chalk.blue(`Server started @ http://${HOST}:${PORT}/`))
 );
