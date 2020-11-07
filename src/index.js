a3bc9889205826fc3b8f1257e64a633383dbc958
mahalakshmi111 <74039533+mahalakshmi111@users.noreply.github.com>
2020-11-07 14:56:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 const chalk = require("chalk");
-const { PORT, HOST } = require("./config");
+const config = require("./config");
+console.log(config)
 const app = require("./app");
 
 const http = require("http");
 const server = http.Server(app);
-console.log(server);
-console.log(PORT);
-console.log(HOST);
-server.listen(PORT, HOST, () =>
-  console.log(chalk.blue(`Server started @ http://${HOST}:${PORT}/`))
+server.listen(config.PORT, config.HOST, () =>
+  console.log(chalk.blue(`Server started @ http://${config.HOST}:${config.PORT}/`))
 );
