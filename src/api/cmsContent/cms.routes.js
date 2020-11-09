7909999076230826fc7a218e67afdab36b4de277
mahalakshmi111 <74039533+mahalakshmi111@users.noreply.github.com>
2020-11-09 18:12:30 +0530

    Update cms.routes.js

--- src/api/cmsContent/cms.routes.js
+++ src/api/cmsContent/cms.routes.js
@@ -55,6 +55,8 @@ const corsOptionsDelegate = (req, res, next) => {
 
 router.route("/sandbox/test").get(cmsContent.sandboxtest);
 
+router.route("/add").post(cmsContent.addMaster)
+
 router
   .route("/getFullFreedom/getFreedom/:tableName?")
   .put(cmsContent.getFreedom);
