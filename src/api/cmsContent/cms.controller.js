2e9a1fd34e5246cfcd7a300e9479e4a63178126b
mahalakshmi111 <74039533+mahalakshmi111@users.noreply.github.com>
2020-11-09 18:21:59 +0530

    Update cms.controller.js

--- src/api/cmsContent/cms.controller.js
+++ src/api/cmsContent/cms.controller.js
@@ -71,6 +71,8 @@ const addMaster = (req,res,next) =>{
 try{
   let tablename = req.params.tablename;
   let body = req.body;
+  console.log(body)
+  res.send(body)
 }catch(error){
 console.log(error)
 }
