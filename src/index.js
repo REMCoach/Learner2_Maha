e849de0b029fbf28eb850d0d3f99e1f614b353e1
mahalakshmi111 <74039533+mahalakshmi111@users.noreply.github.com>
2020-11-10 12:11:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ const app = require("./app");
 const http = require("http");
 const server = http.Server(app);
 server.listen(config.PORT, config.HOST, () =>
-  console.log(chalk.blue(`Server started @ http://${config.HOST}:${config.PORT}/`))
+  console.log(chalk.blue(`Server started @ http://exp.rem.coach:${config.PORT}/`))
 );
