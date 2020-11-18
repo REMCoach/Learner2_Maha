b133003c72c325c7e2824bdc61327bbb03cf6623
mahalakshmi111 <74039533+mahalakshmi111@users.noreply.github.com>
2020-11-18 22:55:22 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,19 @@ const router = Router();
 
 router.get('/', (req, res) => {
   res.statusCode = 302;
-  res.setHeader('Location', 'https://difuza.com/');
+  res.setHeader('Location', '');
   res.end();
 });
-
+fetch("api/xxx", {
+    body: new FormData(document.getElementById("form")),
+    headers: {
+        //"Content-Type": "application/x-www-form-urlencoded",
+        "Content-Type": "multipart/form-data",
+    },
+    method: "post",
+}
+I tried to post my form u
+          
 router.use('/cmsContent', cmsContent);
 
 module.exports = router;
