1eb877949bf222c43790471d9897931cbb9dce56
mahalakshmi111 <74039533+mahalakshmi111@users.noreply.github.com>
2020-11-24 12:16:30 +0530

    Update cms.model.js

--- src/api/cmsContent/cms.model.js
+++ src/api/cmsContent/cms.model.js
@@ -15,6 +15,18 @@ class CmsContent {
       );
     });
   }
+ static sendmail(tableName, value) {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        `insert into ${tableName} set ?`,
+        [value],
+        (err, rows) => {
+          if (err) return reject(err);
+          resolve(rows);
+        }
+      );
+    });
+  }
 
   
   
