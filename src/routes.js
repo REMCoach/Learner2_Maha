1803bb516498bb4a1b73bbe59e91bfe6f34185b1
mahalakshmi111 <74039533+mahalakshmi111@users.noreply.github.com>
2020-12-05 14:38:34 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const cmsContent = require('./api/cmsContent/cms.routes');
 
 const router = Router();
 
-router.get('https://mc0cj.csb.app/', (req, res) => {
+router.get('https://7vzbs.csb.app/', (req, res) => {
   res.statusCode = 302;
   res.setHeader('Location', 'google.com');
   res.end();
